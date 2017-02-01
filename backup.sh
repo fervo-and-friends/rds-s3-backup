e3a4d0503b954790d4676352e0a35be5988b1de1
Magnus Nordlander <magnus@nordlander.se>
2017-02-01 18:03:25 +0100

    Added endpoint

--- backup.sh
+++ backup.sh
@@ -36,7 +36,7 @@ echo -e "\e[1;34m$db\e[00m"
 
 # Dump and zip
 echo -e "  streaming \e[0;35m$object to bucket $S3_BUCKET\e[00m"
-mysqldump -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST --ssl-ca=/app/rds-combined-ca-bundle.pem --force --opt --databases "$db" | bzip2 -c | /app/vendor/gof3r_0.5.0_linux_amd64/gof3r put -b "$S3_BUCKET" -k "$object"
+mysqldump -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST --ssl-ca=/app/rds-combined-ca-bundle.pem --force --opt --databases "$db" | bzip2 -c | /app/vendor/gof3r_0.5.0_linux_amd64/gof3r put -b "$S3_BUCKET" -k "$object" --endpoint=s3-$AWS_DEFAULT_REGION.amazonaws.com
 
 # Jobs a goodun
 echo -e "\e[1;32mDone\e[00m"
