c3398711505642ac8a91b852492e95226e3ea983
Magnus Nordlander <magnus@nordlander.se>
2017-02-01 17:58:48 +0100

    Updated formats and gof3r call

--- backup.sh
+++ backup.sh
@@ -13,7 +13,7 @@
 # * MYSQL_HOST
 # * MYSQL_USER
 # * MYSQL_PASSWORD
-# * S3_PATH
+# * S3_BUCKET
 # * AWS_ACCESS_KEY_ID – AWS access key.
 # * AWS_SECRET_ACCESS_KEY – AWS secret key.
 # * AWS_DEFAULT_REGION – AWS region.
@@ -24,18 +24,19 @@
 db="$1"
 
 # Timestamp (sortable AND readable)
-stamp=`date +"%s - %A %d %B %Y @ %H%M"`
+stamp=`date +"%Y-%m-%d"`
+unix=`date +"%s"`
 
 # Define our filenames
-filename="$db - $stamp.sql.bz2"
-object="$S3_PATH/$stamp/$filename"
+filename="$db - $unix.sql.bz2"
+object="$stamp/$filename"
 
 # Feedback
 echo -e "\e[1;34m$db\e[00m"
 
 # Dump and zip
-echo -e "  streaming \e[0;35m$object\e[00m"
-mysqldump -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST --ssl-ca=/app/rds-combined-ca-bundle.pem --force --opt --databases "$db" | bzip2 -c | /app/vendor/gof3r_0.5.0_linux_amd64/gof3r put "$object"
+echo -e "  streaming \e[0;35m$object to bucket $S3_BUCKET\e[00m"
+mysqldump -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST --ssl-ca=/app/rds-combined-ca-bundle.pem --force --opt --databases "$db" | bzip2 -c | /app/vendor/gof3r_0.5.0_linux_amd64/gof3r put -b "$S3_BUCKET" -k "$object"
 
 # Jobs a goodun
 echo -e "\e[1;32mDone\e[00m"
